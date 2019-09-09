734f280d57e2c5aba347689bd96289ad5b49eca8
rojinshahbazian <rojin.shahbazian@gmail.com>
2019-09-09 13:39:28 -0700

    successfully added the create account fragment :)

--- app/src/main/java/com/example/safehopper/ui/createAccount/CreateAccountFragment.java
+++ app/src/main/java/com/example/safehopper/ui/createAccount/CreateAccountFragment.java
@@ -23,13 +23,13 @@ public class CreateAccountFragment extends Fragment {
         createAccountViewModel =
                 ViewModelProviders.of(this).get(CreateAccountViewModel.class);
         View root = inflater.inflate(R.layout.fragment_create_account, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        createAccountViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        createAccountViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
