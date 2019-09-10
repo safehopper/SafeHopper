5ae4dc5daa063d7bef5725129155defdd3dbdef2
martraj <marielqt98@gmail.com>
2019-09-09 19:56:30 -0700

    Routes List Screen with RecyclerView

--- app/src/main/java/com/example/safehopper/ui/routes/RoutesFragment.java
+++ app/src/main/java/com/example/safehopper/ui/routes/RoutesFragment.java
@@ -1,22 +1,33 @@
 package com.example.safehopper.ui.routes;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import com.example.safehopper.R;
+
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
-import com.example.safehopper.R;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class RoutesFragment extends Fragment {
 
+    private static final String TAG = "RoutesFragment";
+
     private RoutesViewModel routesViewModel;
+
+    private ArrayList<String> rNames = new ArrayList<>();
+    private ArrayList<String> rMiles = new ArrayList<>();
+    private ArrayList<String> rImageURLs = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +41,28 @@ public class RoutesFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Log.d(TAG, "onCreateView: started");
+
+        initImageBitmaps();
+
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerv_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(rNames, rImageURLs, rMiles, this.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         return root;
+    }
+
+    private void initImageBitmaps() {
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps");
+
+        rImageURLs.add("https://cdn-assets.alltrails.com/static-map/production/at-map/20792117/trail-us-california-el-dorado-east-regional-park-perimeter-loop-at-map-20792117-1534368141-414x200-1.png");
+        rNames.add("El Dorado Park");
+        rMiles.add("1.1 miles");
+
+        rImageURLs.add("https://cdn-assets.alltrails.com/static-map/production/at-map/17283855/trail-us-california-eaton-canyon-trail-at-map-17283855-1531801042-414x200-1.png");
+        rNames.add("Eaton Canyon");
+        rMiles.add("3.2 miles");
     }
 }
