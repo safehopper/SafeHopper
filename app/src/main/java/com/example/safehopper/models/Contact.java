23876e5f4c2b89463f32984db0df4a9931096e7e
GBarron93 <42685640+GBarron93@users.noreply.github.com>
2019-10-02 13:24:44 -0700

    Created attributes, setters, getters, and a toString method for the Contact model class.

--- app/src/main/java/com/example/safehopper/models/Contact.java
+++ app/src/main/java/com/example/safehopper/models/Contact.java
@@ -1,45 +1,11 @@
 package com.example.safehopper.models;
 
-public class Contact {
+import android.app.Person;
 
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String email;
+public class Contact extends Person {
+
     private boolean textAlert;
     private boolean emailAlert;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public boolean isTextAlert() {
         return textAlert;
@@ -60,11 +26,7 @@ public class Contact {
     @Override
     public String toString() {
         return "Contact{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                ", textAlert=" + textAlert +
+                "textAlert=" + textAlert +
                 ", emailAlert=" + emailAlert +
                 '}';
     }
