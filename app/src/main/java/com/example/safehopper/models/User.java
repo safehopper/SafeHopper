dc1501acb66421444d64021d2c935f791934e44f
GBarron93 <42685640+GBarron93@users.noreply.github.com>
2019-10-03 13:17:32 -0700

    Created attributes, setters, getters, and a toString method for the Contact model class.

--- app/src/main/java/com/example/safehopper/models/User.java
+++ app/src/main/java/com/example/safehopper/models/User.java
@@ -1,12 +1,8 @@
 package com.example.safehopper.models;
 
-public class User {
+public class User extends Person {
 
     private String password;
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String email;
 
     public String getPassword() {
         return password;
@@ -16,46 +12,14 @@ public class User {
         this.password = password;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String toString() {
         return "User{" +
-                "password='" + password + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
+                "firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", phoneNumber='" + getPhoneNumber() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", password=" + password +
                 '}';
     }
 }
