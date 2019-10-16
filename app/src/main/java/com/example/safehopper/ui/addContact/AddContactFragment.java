ef2bdc7d3bf39a977c5063259785fe102147f3fe
martraj <marielqt98@gmail.com>
2019-10-16 11:38:13 -0700

    User, Routes, and Contacts requests.  Tested using the login button on the login fragment, did not include the code here - let me know if you want the code I used to test.

--- app/src/main/java/com/example/safehopper/ui/addContact/AddContactFragment.java
+++ app/src/main/java/com/example/safehopper/ui/addContact/AddContactFragment.java
@@ -4,15 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.safehopper.R;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 public class AddContactFragment extends Fragment {
 
@@ -30,6 +27,7 @@ public class AddContactFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+
         return root;
     }
 }
