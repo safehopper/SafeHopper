5c53b0f711d77b031d457bf51ff8620ed84c636b
Andrew-Delgado <andrewdelgado017@gmail.com>
2019-11-06 09:41:34 -0800

    Added new buttons to fragment_homepage and session_activity

--- app/src/main/java/com/example/safehopper/SessionActivity.java
+++ app/src/main/java/com/example/safehopper/SessionActivity.java
@@ -12,8 +12,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -52,9 +50,9 @@ public class SessionActivity extends AppCompatActivity implements
         OnMapReadyCallback, GoogleMap.OnPolylineClickListener,
         GoogleMap.OnMapClickListener, GoogleMap.OnMyLocationButtonClickListener, ActivityCompat.OnRequestPermissionsResultCallback, GoogleMap.OnMyLocationClickListener {
 
-    private static final float MIN_DISTANCE = 0;
-    private static final long MIN_TIME = 2000;
-    private static final int TOLERANCE = 100;
+    private static final float  MIN_DISTANCE =    0;
+    private static final long   MIN_TIME     = 2000;
+    private static final int    TOLERANCE    =  100;
 
 
     // Instance Variables
@@ -129,14 +127,6 @@ public class SessionActivity extends AppCompatActivity implements
         polyline1 = mMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .addAll(route.getRouteWaypoints()));//Getting the points from the Route Class
-        // I want to add a marker so the user knows where they initially pressed.
-        // This code only shows a marker after two points.
-        // Fix this in the future.
-        // No justin, I will not remove this unused code. WE NEED IT IN THE FUTURE.
-
-        //        polyline1.setStartCap(
-        //                new CustomCap(
-        //                        BitmapDescriptorFactory.defaultMarker(), 10));
 
         polyline1.setTag("Set the tag of the polyline to what the user names the polyline in the future?");
 
@@ -198,16 +188,6 @@ public class SessionActivity extends AppCompatActivity implements
         });
     }
 
-    private void routeModeToggle() {
-        final Switch s = findViewById(R.id.switch2);
-        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                //make the route snap to roads
-                //make the routes not snap to roads
-            }
-        });
-    }
 
     private String findDistace(List<LatLng> path) {
         // gets the length of the path in meters and converts to feet.
@@ -261,7 +241,7 @@ public class SessionActivity extends AppCompatActivity implements
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
-    // Run once
+    // Runs once
     private void getCurrentLocation() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         fusedLocationClient.getLastLocation()
@@ -306,9 +286,9 @@ public class SessionActivity extends AppCompatActivity implements
                 isOnpath = PolyUtil.isLocationOnPath(new LatLng(location.getLatitude(), location.getLongitude()), polyline1.getPoints(), true, TOLERANCE);
                 pathTaken.addPoint(new LatLng(location.getLatitude(),location.getLongitude()));
 
-
                 setRouteColor();
 
+                // Logic for sending alerts or not.
                 if(sendAlert) {
                         // TODO: Make send alert request
                     Log.d("ALERT",pathTaken.turnToJson());
